ca4bda28f6398b872be3463095528b24952ff5d6
LuValhala <lubos.valco@gmail.com>
2016-08-21 17:17:32 +0200

    Update 4. prevody z dvojkovej a sestnastkovej sustavy.py

--- 4. prevody z dvojkovej a sestnastkovej sustavy.py
+++ 4. prevody z dvojkovej a sestnastkovej sustavy.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 These two funtions are transfering the input numbers from binary and hexadecimery system.
-Ľuboš Valčo, march 2014
+Ľuboš Valčo, march 2015
 """
 
 def dvojkova(binary):
