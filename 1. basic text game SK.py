3eda1ce19c03212fe75b521f7e880abbd3e3b3c0
LuValhala <lubos.valco@gmail.com>
2016-08-21 17:22:09 +0200

    Update 1. basic text game SK.py

--- 1. basic text game SK.py
+++ 1. basic text game SK.py
@@ -4,7 +4,7 @@ Pressing the right keys in console input will lead you to the end of the game.
 This text game was created for school subject and is completely in slovak language.
 Variables in this file are quite random, but were used to hold the cycle before the "key to open the door" was found. 
 After that is achieved, you can continue through the game.
-Ľuboš Valčo, october 2014
+Ľuboš Valčo, october 2015
 """
 print "Zobudil som sa s prazdnou flasou tatranskeho caju 72% v ruke a som v nejakej novej budove. Chcem sa odtial dostat a zistit co sa stalo"
 q = 0
