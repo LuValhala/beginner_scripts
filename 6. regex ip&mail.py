959c62f4e1a5c7b5ddf67a60dcc03a15d60f0361
LuValhala <lubos.valco@gmail.com>
2016-08-21 17:21:17 +0200

    Update 6. regex ip&mail.py

--- 6. regex ip&mail.py
+++ 6. regex ip&mail.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+Sipmle regional expression search and match excercise. Matching the IP and searching for mail.
+Ľuboš Valčo, april 2015
+"""
 import re
-#ip=input("Zadajte IP adresu:")
+#optionaly: ip=input("Zadajte IP adresu:")
 ip = "78.99.245.194"
 findip=re.match(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$",ip)
 if findip:
